10cdc6ecabdde589cdf606765cde5b302754364c
tohin222 <vampirehaunted24@gmail.com>
2021-04-11 11:39:12 +0600

    complete

---
+++ app/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator,MinValueValidator
+# Create your models here.
+
+STATE_CHOICES = (
+    ('dhaka','dhaka'),
+    ('cumilla','cumilla'),
+    ('barishal','barishal'),
+    ('khulna','khulna'),
+    ('coxbazar','coxbazar'),
+    ('Narayganj','Narayganj'),
+)
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES,max_length=50)
+
+def __str__(self):
+    return str(self.id)
+
+CATEGORY_CHOICES = (
+    ('M','Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Waer'),
+    ('BW','Bottom Wear'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discount_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices= CATEGORY_CHOICES,max_length=2)
+    product_image = models.ImageField(upload_to = 'productimg')
+
+def __str__(self):
+    return str(self.id)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discount_price
+
+STATUS_CHOICES = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+)
+
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    order_date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES,default='Pending')
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discount_price
